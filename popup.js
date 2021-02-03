60f4e70bc3ad703bd7e2f963e522fbac9e8ca23e
MHanson15 <hansonmatthew22@gmail.com>
2021-02-03 09:51:39 -0800

    Removed unused function and changed numbering of tabs to start at 1

--- popup.js
+++ popup.js
@@ -1,31 +1,14 @@
-function play(index) {
-	chrome.tabs.query({
-		currentWindow: true}, 
-		function(tabs) {
-			for (var i = 0; i < tabs.length; i++) {
-				if( index == i ) { 
-					chrome.tabs.update(tabs[i].id, {"muted": false}, function(tabs){});
-				}
-				else {
-				chrome.tabs.update(tabs[i].id, {"muted": true}, function(tabs) {});
-				}
-			}
-		}
-	
-	);
-}
-
 chrome.tabs.query({
 currentWindow: true}, 
 function(tabs) {
 	for (var i = 0; i < tabs.length; i++) {
 		if(tabs[i].mutedInfo.muted){
 			document.getElementsByClassName("btn-group")[0].innerHTML
-			+= "<button id='muted'>" + i.toString() + "</button>"	
+			+= "<button id='muted'>" + (i + 1).toString() + "</button>"	
 		}
 		else {
 			document.getElementsByClassName("btn-group")[0].innerHTML
-			+= "<button>" + i.toString() + "</button>"	
+			+= "<button>" + (i + 1).toString() + "</button>"	
 		}
 			
 	}
@@ -34,7 +17,7 @@ function(tabs) {
 document.addEventListener('DOMContentLoaded', function() {
 	wrapper = document.getElementsByClassName("btn-group")[0]
     wrapper.addEventListener('click', function(event){
-		index = parseInt(event.target.innerHTML);
+		index = parseInt(event.target.innerHTML) - 1;
 		chrome.tabs.query({
 			currentWindow: true}, 
 			function(tabs) {
